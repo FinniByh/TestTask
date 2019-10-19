bef36a9b7e8d4a18ceb65a2e27a9fc6e00ab3fc5
finnibyh <gnomdanik@gmail.com>
2019-10-19 23:26:33 +0300

    redesign posts viewing as an example

--- src/TestApp/Components/Post/Post.js
+++ src/TestApp/Components/Post/Post.js
@@ -3,9 +3,12 @@ import React from 'react'
 export default function Post(props) {
   return(
     <div className='post-body'>
-      <p className='post-author'>{`${props.name} is writing in ${props.theme}`}</p>
+      <p className='post-theme'>{props.theme}</p>
       <p className='post-content'>{props.text}</p>
-      <p className='post-date'>{`Post was created in ${props.date}`}</p>
+      <span className='post-info'>
+        <p className='post-date'>{props.date}</p>
+        <p className='post-author'>{props.name}</p>
+      </span>
     </div>
   );
 }
